a8d28d09e0414a72b04da7ecb9e191bc34044618
HannaKaliada <kaledaania2411@gmail.com>
2020-04-08 01:47:23 +0300

    fix: correct alert message

--- script.js
+++ script.js
@@ -271,7 +271,8 @@ const GemPuzzle = {
         }
         // if assemble the puzzle
         if (JSON.stringify(this.properties.gameBlocksContent) === JSON.stringify(mixedBlocks)) {
-          setTimeout(() => alert(`Ура! Вы решили головоломку за ${localStorage.time} и ${localStorage.steps} ходов`), 2000); // eslint-disable-line no-alert
+          setTimeout(() => alert('Ура! Вы решили головоломку за ' + localStorage.time + ' и ' + localStorage.steps + ' ходов'), 1000); // eslint-disable-line no-alert
+          clearInterval(this.properties.time);
         }
       }
     });
